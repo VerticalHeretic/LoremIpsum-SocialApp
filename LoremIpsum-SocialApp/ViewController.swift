d2d2e4de1c3b29c52c49749adca7543780ab01a8
LSWarss <ls.warss98@gmail.com>
2020-11-10 14:28:05 +0100

    Initial Commit

---
+++ LoremIpsum-SocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoremIpsum-SocialApp
+//
+//  Created by Lukasz Stachnik on 10/11/2020.
+//  Copyright © 2020 Warss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoremIpsum-SocialAppTests/LoremIpsum_SocialAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoremIpsum_SocialAppTests.swift
+//  LoremIpsum-SocialAppTests
+//
+//  Created by Lukasz Stachnik on 10/11/2020.
+//  Copyright © 2020 Warss. All rights reserved.
+//
+
+import XCTest
+@testable import LoremIpsum_SocialApp
+
+class LoremIpsum_SocialAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
