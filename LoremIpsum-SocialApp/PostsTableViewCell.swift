aba23ecdf546f8b8024099e29802ae0de1318f9b
LSWarss <ls.warss98@gmail.com>
2020-11-12 08:44:49 +0100

    Added PostsTableViewCell

---
+++ LoremIpsum-SocialApp/PostsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostsTableViewCell.swift
+//  LoremIpsum-SocialApp
+//
+//  Created by Lukasz Stachnik on 12/11/2020.
+//  Copyright © 2020 Warss. All rights reserved.
+//
+
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
