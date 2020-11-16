45f5415658e0ae8b11d50056392079233b4f554e
LSWarss <ls.warss98@gmail.com>
2020-11-16 14:33:20 +0100

    Added map support on user view

--- LoremIpsum-SocialApp/UserDetailsViewController.swift
+++ LoremIpsum-SocialApp/UserDetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class UserDetailsViewController: UIViewController {
 
@@ -15,8 +16,11 @@ class UserDetailsViewController: UIViewController {
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var websiteLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var map: MKMapView!
     
     var user : User!
+    
+    let coordinatesING = CLLocationCoordinate2DMake(50.265450, 19.017950)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,18 @@ class UserDetailsViewController: UIViewController {
         self.emailLabel.text = self.user.email
         self.phoneLabel.text = self.user.phone
         self.websiteLabel.text = self.user.website
+        
+        print("Lat: \(Double(user.address.geo.lat))!")
+        print("Long: \(Double(user.address.geo.lng))!")
+        let centerLocation = CLLocationCoordinate2DMake(Double(user.address.geo.lat)!, Double(user.address.geo.lng)!)
+       
+        let mapSpan = MKCoordinateSpan.init(latitudeDelta:  0.01,longitudeDelta: 0.01)
+        let mapRegion = MKCoordinateRegion.init(center: centerLocation, span: mapSpan)
+        
+        self.map.showsUserLocation = true
+        self.map.setRegion(mapRegion, animated: true)
+        
+        
         // Do any additional setup after loading the view.
     }
     
